ea018f5414813aa42776c4c84bff75e4943b7069
Jason K. Moore <moorepants@gmail.com>
2024-08-29 07:15:54 +0200

    Updated the test to work.

--- pygait2d/tests/test_model.py
+++ pygait2d/tests/test_model.py
@@ -3,6 +3,8 @@
 """This ensures that the PyDy model gives the same result as the Autolev
 model."""
 
+import os
+
 import yaml
 import numpy as np
 from numpy import testing
@@ -12,30 +14,38 @@ from algait2de.gait2de import evaluate_autolev_rhs as autolev_rhs
 # local imports
 from .. import derive, simulate
 
+ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
+
 
 def test_accelerations():
     (mass_matrix, forcing_vector, kane, constants, coordinates, speeds,
-     specified, visualization_frames, ground, origin) = \
-        derive.derive_equations_of_motion()
+     specified, visualization_frames, ground, origin) = derive.derive_equations_of_motion()
 
-    pydy_rhs = generate_ode_function(mass_matrix, forcing_vector, constants,
-                                     coordinates, speeds,
-                                     specified=specified)
+    pydy_rhs = generate_ode_function(
+        forcing_vector,
+        coordinates,
+        speeds,
+        constants=constants,
+        mass_matrix=mass_matrix,
+        specifieds=specified,
+        generator='cython',
+        constants_arg_type='array',
+        specifieds_arg_type='array',
+    )
 
     coordinate_values = np.random.random(9)
     speed_values = np.random.random(9)
     specified_values = np.random.random(9)
 
-    constant_map = simulate.load_constants(constants,
-                                           'data/example_constants.yml')
-    args = {'constants': np.array(constant_map.values()),
-            'specified': specified_values}
+    constant_map = simulate.load_constants(
+        constants, os.path.join(ROOT, 'data/example_constants.yml'))
+    args = (specified_values, np.array(list(constant_map.values())))
 
     x = np.hstack((coordinate_values, speed_values))
-    pydy_xdot = pydy_rhs(x, 0.0, args)
+    pydy_xdot = pydy_rhs(x, 0.0, *args)
 
-    with open('data/example_constants.yml', 'r') as f:
-        constants_dict = yaml.load(f)
+    with open(os.path.join(ROOT, 'data/example_constants.yml'), 'r') as f:
+        constants_dict = yaml.load(f, Loader=yaml.SafeLoader)
 
     constants_dict = simulate.map_values_to_autolev_symbols(constants_dict)
 
